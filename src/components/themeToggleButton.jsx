f1ec1b0b80a2116c7cb1cb519da49be60f4f4194
Booker King <btking1@my.waketech.edu>
2022-10-01 13:01:28 -0400

    .

--- src/components/themeToggleButton.jsx
+++ src/components/themeToggleButton.jsx
@@ -39,7 +39,7 @@ export default function ThemeToggle() {
   }, [])
 
   return isMounted ? (
-    <div className="inline-flex items-center p-[1px] rounded-3xl bg-orange-300 dark:bg-zinc-600">
+    <div className="inline-flex items-center p-[1px] rounded-3xl bg-blue-400  dark:bg-zinc-600 dark:text-blue-400">
       {themes.map(t => {
         const checked = t === theme
         return (

5260c3066e7cbd167d1c310c8bd4daebee650bb0
Booker King <btking1@my.waketech.edu>
2022-09-30 22:09:39 -0400

    deploy to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), sitemap()],
-  output: "server",
+
   adapter: vercel(),
 });

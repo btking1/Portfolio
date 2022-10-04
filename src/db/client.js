fb674535798686a7bf784b5ab41b96a7f48ee9ee
Booker King <btking1@my.waketech.edu>
2022-10-04 19:00:11 -0400

    config astro

--- src/db/client.js
+++ src/db/client.js
@@ -1,10 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-
 export const prisma =
   global.prisma ||
   new PrismaClient({
     log: ["query", "info", "warn"],
   });
 
-if (process.env.NODE_ENV === "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
